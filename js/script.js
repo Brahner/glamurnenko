4ea05f9575be4156180254b7baa22dbc53da5218
Brahner <brachneq@gmail.com>
2022-11-24 19:27:33 +0400

    add show header scroll

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	const sidebar = document.querySelector('.sidebar'),
-	container = document.querySelector('#sidebar__container'),
+	container = document.querySelector('.sidebar__container'),
 	sidebarItem = document.querySelectorAll('.sidebar__item'),
-	burger = document.querySelector('.burger');
+	burger = document.querySelector('.burger'),
+	header = document.querySelector('.header'),
+	promo = document.querySelector('.promo');
 	document.querySelector('#scroll').addEventListener('click', scrollToElement);
 
 	burger.addEventListener('click', () => {
 		sidebar.classList.toggle('sidebar--active');
 		container.classList.toggle('sidebar__container--active');
 	});
-
 
 	sidebar.onclick = function (e) {
 		const withinContainer = e.composedPath().includes(container);
@@ -26,9 +27,20 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	};
 
-	
 	function scrollToElement(e) {
-		let element = document.getElementById("promo");
-		element.scrollIntoView(true);
+		promo.scrollIntoView(true);
 	}
+
+	showHeaderScroll = () =>{
+		const currentScroll = window.pageYOffset; 
+		let elementID = 'main';
+		const elementOffsetTop = document.getElementById(elementID).offsetTop;
+	
+		if ( currentScroll > elementOffsetTop && header.className != 'header--stiky'){
+			header.classList.add('header--stiky');
+		} else {
+			header.classList.remove('header--stiky');
+		}
+	};
+	window.addEventListener('scroll', showHeaderScroll);
 });
