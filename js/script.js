7354be25ca095e5d27f1e3908d51ce64e33953ba
Brahner <brachneq@gmail.com>
2022-11-25 15:06:28 +0400

    minor fix sidebar

--- js/script.js
+++ js/script.js
@@ -16,13 +16,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	sidebar.onclick = function (e) {
 		const withinContainer = e.composedPath().includes(container);
 		if ( ! withinContainer ) {
-			sidebar.classList.toggle('sidebar--active');
-			container.classList.toggle('sidebar__container--active');
+			sidebar.classList.remove('sidebar--active');
+			container.classList.remove('sidebar__container--active');
 		}
 		sidebarItem.forEach(item => {
 			item.addEventListener('click', () => {
-				sidebar.classList.toggle('sidebar--active');
-				container.classList.toggle('sidebar__container--active');
+				sidebar.classList.remove('sidebar--active');
+				container.classList.remove('sidebar__container--active');
 			});
 		});
 	};
